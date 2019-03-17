5cbe981d58d71d91cf902a2392007f005d1e9a59
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-03-17 14:26:14 -0500

    Added another possibility for semantic searcher

--- src/main/java/com/jeeps/gov_semantic_searcher/utils/SearchParser.java
+++ src/main/java/com/jeeps/gov_semantic_searcher/utils/SearchParser.java
@@ -5,12 +5,15 @@ public class SearchParser {
     public static int parseSearch(String q) {
         if (q.contains("cuantos") || q.contains("cuenta"))
             return 1;
-        if (q.contains("pertenece a"))
+        if (q.contains("pertenece a") || q.contains("pertenezcan a"))
             return 2;
         return 0;
     }
 
     public static String extractGroup(String q) {
-        return q.split("pertenece a ")[1].trim();
+        if (q.contains("pertenece a"))
+            return q.split("pertenece a ")[1].trim();
+        else
+            return q.split("pertenezcan a ")[1].trim();
     }
 }
