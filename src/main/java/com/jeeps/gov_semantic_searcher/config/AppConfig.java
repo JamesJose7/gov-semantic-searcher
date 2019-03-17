d79b328752ec48616b4d4bbd04730cbc2ced837b
Jose Eduardo Eguiguren <pepe.96.jose@gmail.com>
2019-03-17 01:24:49 -0500

    d3sparql graph and search form

---
+++ src/main/java/com/jeeps/gov_semantic_searcher/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.jeeps.gov_semantic_searcher.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class AppConfig {
+}
--- src/main/java/com/jeeps/gov_semantic_searcher/web/controller/SearcherController.java
+++ src/main/java/com/jeeps/gov_semantic_searcher/web/controller/SearcherController.java
@@ -2,13 +2,24 @@ package com.jeeps.gov_semantic_searcher.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.websocket.server.PathParam;
+
 @Controller
+@CrossOrigin(origins = "http://localhost:8890")
 public class SearcherController {
 
     @RequestMapping("/")
     public String semanticSearcher(Model model) {
         return "index";
+    }
+
+    @RequestMapping("/search")
+    public String searchResult(@PathParam("q") String q, Model model) {
+        System.out.println(q);
+        return "result";
     }
 }
